d3d692ba62bd3484bde21799b976d6b5ec5d8309
Ian <cupofcoffeewithlime1@gmail.com>
2022-06-01 22:39:19 +0300

    dataclass for triangle module

--- triangle/triangle.py
+++ triangle/triangle.py
@@ -1,20 +1,17 @@
+from dataclasses import dataclass
+
 from geometry import Figure
 from point import Point
 
 from . import validations
 
 
+@dataclass()
 class Triangle(Figure):
     validation_class = validations.TriangleValidation
-
-    def __init__(self, a: Point, b: Point, c: Point):
-        super().__init__(a, b, c)
-        self.a = a
-        self.b = b
-        self.c = c
-
-    def __repr__(self) -> str:
-        return f"A{self.a}, B{self.b}, C{self.c}"
+    a: Point
+    b: Point
+    c: Point
 
 
 class RightTriangle(Triangle):
